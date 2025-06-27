be94755c96f2812246982b6853d96b5873ee696d
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-06-27 19:47:08 +0530

    :zap: :bulb: Logo in header added

--- components/main-header/main-header.tsx
+++ components/main-header/main-header.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import NavLinks from "../nav-links/nav-links";
 import classes from "./main-header.module.css";
+import logo from "@/public/images/logo.png";
+import Image from "next/image";
 
 export default function MainHeader() {
   return (
     <header className={classes.header}>
       {/* logo */}
       <Link href="/" className={classes.logo}>
-        Ganpati Logo
+        <Image src={logo} alt="logo" height={50} width={50} />
       </Link>
 
       {/* navigation */}
