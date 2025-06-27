a2052d658667a8067c26cec574dff8b1aecfecb5
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-06-27 16:14:10 +0530

    :zap: :art: :lipstick: Banner design

--- components/hero/hero-banner.tsx
+++ components/hero/hero-banner.tsx
@@ -1,14 +1,24 @@
+import Image from "next/image";
 import classes from "./hero-banner.module.css";
+import Hero1 from "../../public/images/hero-image-2.png";
 
 export default function HeroBanner() {
   return (
     <section className={classes.banner}>
       <div className={classes.heroText}>
-        <h2 className={classes.text}>
-          Bring Home Divinity This Ganesh Chaturthi
+        <h2 className={classes.heading}>
+          Bring Home <br /> Divinity This <br />
+          Ganesh Chaturthi
         </h2>
+        <p className={classes.subheading}>
+          Handcrafted Eco-friendly Ganapti <br />
+          Murti&apos;s Delivered to Your Doorstep
+        </p>
+        <button className={classes.cta}>Browse Murti&apos;s</button>
       </div>
-      <div className={classes.heroImage}></div>
+      <div className={classes.heroImage}>
+        <Image src={Hero1} alt="hero1" height={500} width={500} />
+      </div>
     </section>
   );
 }
